5ae2772ccdc947546af2f7cc7431712330ab88c7
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-17 07:27:03 +0800

    QS Panel Styles: Fix thinline QS style
    * 10th QS style doesn't exist and thinline's QS style value is 9
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileViewImpl.kt
@@ -1083,7 +1083,7 @@ constructor(
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorUnavailable
             state == Tile.STATE_ACTIVE -> 
-                if (qsPanelStyle == 2 || qsPanelStyle == 10) 
+                if (qsPanelStyle == 2 || qsPanelStyle == 9) 
                     colorActiveAlpha 
                 else if (qsPanelStyle == 3) 
                     colorActiveRandom 
@@ -1104,7 +1104,7 @@ constructor(
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorLabelUnavailable
             state == Tile.STATE_ACTIVE -> 
-                if (qsPanelStyle == 1 || qsPanelStyle == 2 || qsPanelStyle == 10)
+                if (qsPanelStyle == 1 || qsPanelStyle == 2 || qsPanelStyle == 9)
                     colorActive
                 else if (qsPanelStyle == 3) 
                     colorLabelActiveRandom
@@ -1123,7 +1123,7 @@ constructor(
         return when {
             state == Tile.STATE_UNAVAILABLE || disabledByPolicy -> colorSecondaryLabelUnavailable
             state == Tile.STATE_ACTIVE -> 
-                if(qsPanelStyle == 1 || qsPanelStyle == 2 || qsPanelStyle == 10) 
+                if(qsPanelStyle == 1 || qsPanelStyle == 2 || qsPanelStyle == 9) 
                     colorActive
                 else if(qsPanelStyle == 3) 
                     colorSecondaryLabelActiveRandom
